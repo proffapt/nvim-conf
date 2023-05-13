107e13fc461c73c172b9db1ab6ff5fd2e780c738
proffapt <proffapt@pm.me>
2023-05-13 17:31:04 +0530

    update

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>l"] = {"<cmd> :LiveServer <CR>", "Toggle LiveServer on/off"},
 
     -- Buffer management
     ["<leader>q"] = {"<cmd> q! <CR>", "exit current tab"},
